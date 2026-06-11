a8bc24d77ee6d77bfbd00b6231e0734b1c4f62ab
Olletall <olle.tallhage@gmail.com>
2026-06-11 19:37:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const searchInput = document.getElementById("searchInput");
+const gameCards = document.querySelectorAll(".game-card");
+
+searchInput.addEventListener("input", () => {
+  const searchText = searchInput.value.toLowerCase();
+
+  gameCards.forEach(card => {
+    const gameName = card.dataset.name;
+
+    if (gameName.includes(searchText)) {
+      card.style.display = "block";
+    } else {
+      card.style.display = "none";
+    }
+  });
+});
 
